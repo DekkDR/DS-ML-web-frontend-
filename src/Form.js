ac49b6861aec32b3ca29c650dbe0acf72abd2875
Direk <dekk.dtc@gmail.com>
2022-08-28 14:20:47 +0700

    first commit

--- src/Form.js
+++ src/Form.js
@@ -1,28 +1,88 @@
 import {useState} from 'react';
+import './Form.css'
 
 function Form() {
-    const [form, setForm] = useState({name: "", email: ""}); //save form using useState
+    const [form, setForm] = useState({
+        pregnancies: "", 
+        glucose: "",
+        blood_pressure: "",
+        skin_thickness: "",
+        insulin_level: "",
+        bmi: "",
+        diabetes_pedigree: "",
+        age: ""
+    }); 
+
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState("");
+
     const handleSubmit = (event) => {
-        event.preventDefault(); // prevent "Form submitted" disappear in short time.
-        console.log("Form submitted");
-        console.log(form)
+        event.preventDefault(); 
+
+        const form_data = new FormData(); //other types are JSON etc.
+        form_data.append("1", form.pregnancies);
+        form_data.append("2", form.glucose);
+        form_data.append("3", form.blood_pressure);
+        form_data.append("4", form.skin_thickness);
+        form_data.append("5", form.insulin_level);
+        form_data.append("6", form.bmi);
+        form_data.append("7", form.diabetes_pedigree);
+        form_data.append("8", form.age);
+
+        setLoading(true);
+
+        // console.log("form submitted")
+        // console.log(form)
+
+        fetch('https://dsmodeldeployv1.herokuapp.com/predict', {
+            method: 'POST',
+            body: form_data
+        })
+        // .then(response => console.log(response))
+        .then(response => response.text())
+        .then(html => {
+            setResult(html);
+            setLoading(false);
+        })
     };
 
     const onChange = (event) => {
-        console.log("Changed input field")
         const name = event.target.name;
         const value = event.target.value;
+        setForm({ ...form, [name]: value }); 
+    };
 
-        console.log(`event.target.name: ${name}, event.target.value: ${value}`);
+    const handleClear = () => {
+        setForm({
+            pregnancies: "", 
+            glucose: "",
+            blood_pressure: "",
+            skin_thickness: "",
+            insulin_level: "",
+            bmi: "",
+            diabetes_pedigree: "",
+            age: ""
+        });
 
-        setForm({ ...form, [name]: value }); // ...form is for passing previous form value
-    }
+        setResult("");
+    };
 
     return (
         <form onSubmit = {handleSubmit}>
-            <input type = "text" name = "name" onChange = {onChange} placeholder="Name (e.g. John)" />
-            <input type = "email" name = "email" onChange = {onChange} placeholder="Email (e.g. john.doe@example.coom" />
-            <button type = "submit">Submit Form</button>
+            <h4>Diabetes Prediction Model</h4>
+            <p>Example to Predict Probability of Diabetes</p>
+            <input type = "number" name = "pregnancies" value={form.pregnancies} onChange = {onChange} placeholder="Number of Pregnancies" required />
+            <input type = "number" name = "glucose" value={form.glucose} onChange = {onChange} placeholder="Glucose level in Sugar" required />
+            <input type = "number" name = "blood_pressure" value={form.blood_pressure} onChange = {onChange} placeholder="Blood Pressure" required />
+            <input type = "number" name = "skin_thickness" value={form.skin_thickness} onChange = {onChange} placeholder="Skin Thickness" required />
+            <input type = "number" name = "insulin_level" value={form.insulin_level} onChange = {onChange} placeholder="Insulin Level" required />
+            <input type = "number" name = "bmi" value={form.bmi} onChange = {onChange} placeholder="Body Mass Index (BMI)" required />
+            <input type = "number" name = "diabetes_pedigree" value={form.diabetes_pedigree} onChange = {onChange} placeholder="Diabetes Pedigree Function" required />
+            <input type = "number" name = "age" value={form.age} onChange = {onChange} placeholder="Age" required />
+            <button type = "submit" disabled = {loading}>{loading ? "Predicting Result..." : "Submit Form"}</button>
+            {result && <span onClick={handleClear}>Clear Prediction</span>}
+
+            {result && <div dangerouslySetInnerHTML={{ __html: result }} className = 'result' />}
         </form>
     );
 }
